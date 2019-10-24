cbb4b359c019070db0b6bb0d395823b8e6c21684
zhjibi742626083 <jinbiao@bianjie.ai>
2019-10-24 17:51:40 +0800

    add callback error

--- faucet/index.js
+++ faucet/index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const Crypto = require('irisnet-crypto')
-const request = require('request')
+const express = require('express');
+const Crypto = require('irisnet-crypto');
+const request = require('request');
 const http = require('http');
 const qs = require('querystring');
-const app = express()
-const config = require("./config")
+const app = express();
+const config = require("./config");
 
 
 
@@ -14,46 +14,45 @@ const config = require("./config")
 let irisAddressInformation = Crypto.getCrypto('iris','testnet').recover(`${config.app.irisMnemonicWord}`);
 //cosmos 水龙头地址信息
 let commosAddressInformation = Crypto.getCrypto('cosmos','testnet').recover(`${config.app.cosmosMnemonicWord}`);
-
 let iris_account_number,iris_sequence,cosmos_account_number,cosmos_sequence;
 function getAccountNumberAndSequence(){
 	let irisUrl = `${config.app.irisLcdUrl}/auth/accounts/${irisAddressInformation.address}`;
 	let cosmosUrl = `${config.app.cosmosLcdUrl}/auth/accounts/${commosAddressInformation.address}`;
 	request.get(irisUrl,(error, response, body) => {
-			let parseBody = JSON.parse(body)
-			iris_account_number = Number(parseBody.result.value.account_number)
+			let parseBody = JSON.parse(body);
+			iris_account_number = Number(parseBody.result.value.account_number);
 			iris_sequence = Number(parseBody.result.value.sequence)
-		})
+		});
 	request.get(cosmosUrl,(error, response, body) => {
-		let parseBody = JSON.parse(body)
-		cosmos_account_number = Number(parseBody.result.value.account_number)
+		let parseBody = JSON.parse(body);
+		cosmos_account_number = Number(parseBody.result.value.account_number);
 		cosmos_sequence = Number(parseBody.result.value.sequence)
 	})
 }
-getAccountNumberAndSequence()
+getAccountNumberAndSequence();
 app.get('/api/faucet',(req,res) => {
 	let chainId,from,gas,fees,memo,account_number,sequence,denom,AddressInformationPrivateKey,url;
 	if(req.query.chainName === "iris"){
-		chainId = `${config.app.irisChainId}`,
-		from = irisAddressInformation.address,
-		gas = 250000,
-		fees = {denom: "uiris", amount: 50000}
+		chainId = `${config.app.irisChainId}`;
+		from = irisAddressInformation.address;
+		gas = `${config.app.gasNumber}`;
+		fees = {denom: "uiris", amount: 100000};
 		memo = '';
-		account_number = iris_account_number
-		sequence = iris_sequence
-		denom = 'uiris'
-		AddressInformationPrivateKey = irisAddressInformation.privateKey
+		account_number = iris_account_number;
+		sequence = iris_sequence;
+		denom = 'uiris';
+		AddressInformationPrivateKey = irisAddressInformation.privateKey;
 		url = `${config.app.irisLcdUrl}/txs`
 	}else if(req.query.chainName === "cosmos"){
-		chainId = `${config.app.cosmosChainId}`,
-		from = commosAddressInformation.address,
-		gas = `${config.app.gasNumber}`,
-		fees = {denom: "uatom", amount: 50000}
+		chainId = `${config.app.cosmosChainId}`;
+		from = commosAddressInformation.address;
+		gas = `${config.app.gasNumber}`;
+		fees = {denom: "uatom", amount: 100000};
 		memo = '';
-		account_number = cosmos_account_number
-		sequence = cosmos_sequence
-		denom = 'uatom'
-		AddressInformationPrivateKey = commosAddressInformation.privateKey
+		account_number = cosmos_account_number;
+		sequence = cosmos_sequence;
+		denom = 'uatom';
+		AddressInformationPrivateKey = commosAddressInformation.privateKey;
 		url = `${config.app.cosmosLcdUrl}/txs`
 	}
 
@@ -79,25 +78,30 @@ app.get('/api/faucet',(req,res) => {
 	let builder = Crypto.getBuilder(req.query.chainName,'testnet');
 	let signTx = builder.buildAndSignTx(tx,AddressInformationPrivateKey);
 	let postTx = signTx.GetData();
-	postTx.mode='block'
+	postTx.mode='block';
 	request({
 		url: url,
 		method: "POST",
 		json:true,
 		body: postTx
 	}, (error, response, body) => {
-		if (!error && response.statusCode == 200) {
+		if(error){
+			res.send({
+				code: 0,
+				msg:error
+			})
+		}else if (!error && response.statusCode == 200) {
 			if(body && body.logs && body.logs[0].success){
 				res.send({
 					code: 1,
 					msg:'success'
-				})
+				});
 				getAccountNumberAndSequence()
 			}else {
 				res.send({
 					code: 0,
 					msg:'failed'
-				})
+				});
 				getAccountNumberAndSequence()
 			}
 		}
@@ -105,4 +109,4 @@ app.get('/api/faucet',(req,res) => {
 
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
